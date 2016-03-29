f6b4248f10329dcbd6b26705be6e5f9b0a3f14ba
Rustin Low <rustinlow@Rustins-MacBook-Pro.local>
2016-03-29 17:38:57 -0400

    set up basic UI

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,26 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Rustin Low on 3/29/16.
+//  Copyright © 2016 Rustin Low. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
